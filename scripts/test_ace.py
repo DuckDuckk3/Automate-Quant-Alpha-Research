8b5d35eab2509853d1f311a8d18175e26a815dac
Picky_Ducky <nguyenjunior98@gmail.com>
2026-09-20 22:33:07 +0700

    Update test_ace.py

--- scripts/test_ace.py
+++ scripts/test_ace.py
@@ -1,1 +1,27 @@
+import sys
+from pathlib import Path
 
+
+ROOT = Path(__file__).resolve().parents[1]
+ACE_DIR = ROOT / "ace"
+
+sys.path.insert(0, str(ACE_DIR))
+
+
+import ace_lib as ace
+
+
+print("ACE import successful.")
+
+
+print()
+print("Starting BRAIN session...")
+
+s = ace.start_session()
+
+print("Session created.")
+
+
+s = ace.check_session_and_relogin(s)
+
+print("Session authenticated successfully.")
